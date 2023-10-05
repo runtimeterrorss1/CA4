a9f8125482cce428d8cb23fe2ee0f2fe9ae62ec1
runtimeterrorss1 <i200634@nu.edu.pk>
2023-10-06 00:09:00 +0500

    update

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
-FROM python
+FROM python:3.8-slim
 WORKDIR /app
 COPY . /app
+RUN pip install -r requirements.txt
 ENV FLASK_APP=app.py
 EXPOSE 5000
 CMD ["flask", "run", "--host", "0.0.0.0"]
--- app.py
+++ app.py
@@ -15,30 +15,22 @@ def create_db_connection():
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        # Handle the form submission as previously mentioned
 
         try:
-            # Create a database connection
             db = create_db_connection()
 
-            # Create a cursor object to interact with the database
             cursor = db.cursor()
 
-            # Execute a SELECT query
             cursor.execute("SELECT * FROM user")
 
-            # Fetch the results
             results = cursor.fetchall()
 
-            # Close the cursor and database connection
             cursor.close()
             db.close()
 
-            # Pass the results to the template and render it
             return render_template("index.html", results=results)
 
         except mysql.connector.Error as err:
-            # Handle any MySQL errors here
             return f"Error: {err}"
 
 if __name__ == '__main__':
